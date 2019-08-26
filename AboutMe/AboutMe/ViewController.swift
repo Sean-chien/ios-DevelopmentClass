c29c6b2d8dee4cfc6d45bdbd5cafbbf8777f3cfe
Sea <bnly22335@gmail.com>
2019-08-26 14:24:52 -0700

    aboutme

---
+++ AboutMe/AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Sean Chien on 2019-08-26.
+//  Copyright © 2019 Sean Chien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
